29cd4bd8dd5f53ecde705e21bdab9ede3a1f50e1
Gustavo Henrique <Gustavohaa7@gmail.com>
2024-07-24 16:35:16 -0300

    Projeto criado

---
+++ src/test/java/com/educandoweb/course/demo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course.demo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
